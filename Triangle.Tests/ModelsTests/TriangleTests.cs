ddbbfce80407e29982832a7fcac53394af8a626c
Liam Eller <liamthelastson@gmail.com>
2022-02-22 11:15:07 -0800

    pass else statement in our IsItATriangle method
    TheThreeTriangles testTheThreeTriangles = new TheThreeTriangles();

--- Triangle.Tests/ModelsTests/TriangleTests.cs
+++ Triangle.Tests/ModelsTests/TriangleTests.cs
@@ -6,11 +6,18 @@ namespace Triangle.Tests
   [TestClass]
   public class TheThreeTrianglesTests
   {
+    TheThreeTriangles testTheThreeTriangles = new TheThreeTriangles();
+    
+    [TestMethod]
+    public void IsItATriangle_TwoSidesLessThanOther_String()
+    {
+      Assert.AreEqual("it is not a triangle", testTheThreeTriangles.IsItATriangle(1, 2, 5));
+    }
+
     [TestMethod]
     public void IsItATriangle_TwoSidesGreaterThanOther_String()
     {
-      TheThreeTriangles testTheThreeTriangles = new TheThreeTriangles();
-      Assert.AreEqual("it is not a triangle", testTheThreeTriangles.IsItATriangle(1, 2, 5));
+      Assert.AreEqual("it is a type of triangle", testTheThreeTriangles.IsItATriangle(3,3,4));
     }
   }
 }
